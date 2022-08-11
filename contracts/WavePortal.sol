b482a34bdb8e17ec71a5285f30ea3845392963ff
goforpordev <goforprodev@gmail.com>
2022-08-11 23:17:14 +0100

    Modified runjs file

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,21 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    mapping(address => uint256) public wavers;
+
     constructor() {
         console.log("Hello World this is a contract");
+    }
+
+    function wave() public {
+        totalWaves++;
+        console.log("%s waved", msg.sender);
+        wavers[msg.sender] = wavers[msg.sender] + 1;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d waves", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,23 @@
 const main = async () => {
   // HRE -> Hardhat runtime environment
+  const [owner, randomAccount] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
   console.log("Contract deployed to: " + waveContract.address);
+  console.log(`Contract deployed by: ${owner.address}`);
+
+  let waveCounts;
+  waveCounts = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  //   check wave count again after wave
+  waveCounts = await waveContract.getTotalWaves();
+
+  let wavers = await waveContract.wavers;
+  console.log(wavers);
 };
 
 const runMain = async () => {
