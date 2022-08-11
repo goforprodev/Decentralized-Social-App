c492769fdb37f78e616c91cd85c989e3934f01cb
goforpordev <goforprodev@gmail.com>
2022-08-11 22:24:16 +0100

    Created wave-portal contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-Liscence-Identifier: UNLICENSE
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Hello World this is a contract");
+    }
+}
